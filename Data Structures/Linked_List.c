4db2dbff7c8173dec17b6e09310aaf223ec571cc
sener21 <ali.sener@webmarc.io>
2022-05-22 20:55:58 +0300

    feat: build and print linked list

--- Data Structures/Linked_List.c
+++ Data Structures/Linked_List.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+struct Node* buildLinkedList() {
+	// Initializarea nodurilor din lista
+	struct Node* head = NULL;
+	struct Node* second = NULL;
+	struct Node* third = NULL;
+
+	// Alocarea memoriei in HEAP
+	head = (struct Node*)malloc(sizeof(struct Node));
+	second = (struct Node*)malloc(sizeof(struct Node));
+	third = (struct Node*)malloc(sizeof(struct Node));
+
+	// Atribuire de valori si legarea nodurilor intre ele
+	head->data = 1;
+	head->next = second;
+
+	second->data = 2;
+	second->next = third;
+
+	third->data = 3;
+	third->next = NULL;
+
+	return head;
+}
+
+void printLinkedList(struct Node* node) {
+	while (node != NULL) {
+		printf(" %d ", node->data);
+		node = node->next;
+	}
+}
 
 void main() {
-	printf("App running");
+	struct Node* head = buildLinkedList();
+
+	printLinkedList(head);
 }
