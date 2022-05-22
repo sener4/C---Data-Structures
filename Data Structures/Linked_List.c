1effc27054cc351cb0a9dead7f1d1a8193f71ad8
sener21 <ali.sener@webmarc.io>
2022-05-22 21:31:23 +0300

    feat: insert nodes

--- Data Structures/Linked_List.c
+++ Data Structures/Linked_List.c
@@ -6,6 +6,13 @@ struct Node {
 	struct Node* next;
 };
 
+int checkIfNodeExists(struct Node* node) {
+	if (node == NULL) {
+		printf("The node is NULL");
+		return;
+	}
+}
+
 struct Node* buildLinkedList() {
 	struct Node* head = NULL;
 	struct Node* second = NULL;
@@ -32,10 +39,64 @@ void printLinkedList(struct Node* node) {
 		printf(" %d ", node->data);
 		node = node->next;
 	}
+
+	printf("\n");
+}
+
+void insertNodeFront(struct Node** headPointer, int newNodeData) {
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+	newNode->data = newNodeData;
+	newNode->next = (*headPointer);
+
+	(*headPointer) = newNode;
+}
+
+void insertNodeAfter(struct Node* prevNode, int newNodeData) {
+	checkIfNodeExists(prevNode);
+
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+	newNode->data = newNodeData;
+	newNode->next = prevNode->next;
+
+	prevNode->next = newNode;
+}
+
+void insertNodeEnd(struct Node** headPointer, int newNodeData) {
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+	struct Node* last = *headPointer;
+
+	newNode->data = newNodeData;
+	newNode->next = NULL;
+
+	if (*headPointer == NULL) {
+		*headPointer = newNode;
+		return;
+	}
+
+	while (last->next != NULL) {
+		last = last->next;
+	}
+
+	last->next = newNode;
 }
 
 void main() {
 	struct Node* head = buildLinkedList();
+
+	printLinkedList(head);
+
+	insertNodeFront(&head, 0);
+
+	printLinkedList(head);
+
+	insertNodeAfter(head, 5);
+
+	printLinkedList(head);
+
+	insertNodeEnd(&head, 7);
 
 	printLinkedList(head);
 }
